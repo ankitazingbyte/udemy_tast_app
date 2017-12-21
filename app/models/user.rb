eac0dcfbbef8d01d8181cb939be4ab10fe360dba
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-21 15:11:44 +0530

    add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_many :articles
+	has_many :comments
 	before_save { self.email = email.downcase }
 	validates :username, presence: true, 
 			  uniqueness: { case_sensitive: false }, 
@@ -8,5 +9,5 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: {maximum: 105},
 	          uniqueness: { case_sensitive: false},
 	          format: { with: VALID_EMAIL_REGEX}
-	has_many :comments
+	has_secure_password
 end
