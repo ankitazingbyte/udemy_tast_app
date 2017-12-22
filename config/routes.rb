bd07c2ab2d4b9349308a500271b36c52b9864cfa
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-22 15:17:01 +0530

    signup user from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
 	root 'welcome#index'
 
   	resources :comments
-  	resources :users
   	resources :articles
   	get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :categories, except: [:destroy]
-
+	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
 
 	
 end
