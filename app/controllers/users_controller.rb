cf676635eda5a4e41ac289b9fb284128bf5c894e
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-22 16:10:09 +0530

    update current_user to be article creater

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,75 +1,89 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :show]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.paginate(page: params[:page], per_page: 5)
-  end
+before_action :require_same_user, only: [:edit, :update]
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
+def index
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
+@users = User.paginate(page: params[:page], per_page: 5)
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to articles_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
 end
+
+def new
+
+@user = User.new
+
+end
+
+def create
+
+@user = User.new(user_params)
+
+if @user.save
+
+flash[:success] = "Welcome to the alpha blog #{@user.username}"
+
+redirect_to articles_path
+
+else
+
+render 'new'
+
+end
+
+end
+
+def edit
+
+end
+
+def update
+
+if @user.update(user_params)
+
+flash[:success] = "Your account was updated successfully"
+
+redirect_to articles_path
+
+else
+
+render 'edit'
+
+end
+
+end
+
+def show
+
+@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+
+end
+
+private
+
+def user_params
+
+params.require(:user).permit(:username, :email, :password)
+
+end
+
+def set_user
+
+@user = User.find(params[:id])
+
+end
+
+def require_same_user
+
+if current_user != @user
+
+flash[:danger] = "You can only edit your own account"
+
+redirect_to root_path
+
+end
+
+end
+
+end
+
